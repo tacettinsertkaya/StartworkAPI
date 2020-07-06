9aa9780c14e0fe3362a8464e222d595bce0b45ed
ihalilsungur <ihalilsungur@gmail.com>
2020-07-06 16:22:57 +0300

    startwork api projemize swagger ekledik

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -7,17 +7,25 @@ import {
 } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { LoginDto, RegisterDto } from 'src/models/user.model';
+import {  ApiCreatedResponse, ApiOkResponse, ApiUnauthorizedResponse, ApiBody } from "@nestjs/swagger";
 
 @Controller('users')
 export class AuthController {
   constructor(private authService: AuthService) {}
 
   @Post("/register")
+  @ApiCreatedResponse({
+    description:"User Registration"
+  })
+  @ApiBody({type:RegisterDto})
   register(@Body(ValidationPipe) credentials: RegisterDto) {
     return this.authService.register(credentials);
   }
 
   @Post('/login')
+  @ApiOkResponse({description:"User Login"})
+  @ApiUnauthorizedResponse({description:"Invalid credentials"})
+  @ApiBody({type:LoginDto})
   login(@Body(ValidationPipe) credentials: LoginDto) {
     return this.authService.login(credentials);
   }
--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,18 @@
 require('dotenv').config()
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.setGlobalPrefix('api');
+  const options = new DocumentBuilder()
+  .setTitle('Startwork api')
+  .setDescription('Startwork api')
+  .setVersion('1.0.0')
+  .build();
+const document = SwaggerModule.createDocument(app, options);
+SwaggerModule.setup('api', app, document);
   await app.listen(process.env.PORT || 4000);
 }
 bootstrap();
