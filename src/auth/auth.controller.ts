93ea12d6511e03d74efefce02927a73fcbcd6ac3
ihalilsungur <ihalilsungur@gmail.com>
2020-08-01 00:54:01 +0300

    güncellendi

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,6 +1,7 @@
-/* eslint-disable @typescript-eslint/no-empty-function */
 /* eslint-disable @typescript-eslint/no-unused-vars */
+/* eslint-disable @typescript-eslint/no-empty-function */
 /* eslint-disable @typescript-eslint/explicit-module-boundary-types */
+
 import {
   Controller,
   Post,
