d4155c8b63f9ff6a3fd5ad96d173c035825e34e9
tacettin sertkaya <TacettinSertkaya@outlook.com>
2020-08-01 01:57:28 +0300

    Db moved server

--- src/config/typeOrmConfig.ts
+++ src/config/typeOrmConfig.ts
@@ -2,14 +2,14 @@ import { TypeOrmModuleOptions } from '@nestjs/typeorm';
 
 export const typeOrmConfig: TypeOrmModuleOptions = {
   type: 'mysql',
-  host: 'localhost',
+  host: '37.230.108.234',
   port: 3306,
-  username: 'root',
-  password: '12345',
-  database: 'nest_security',
+  username: 'dev_user',
+  password: 'Ts&912513',
+  database: 'tsertka1_startDB',
   entities: [__dirname + '/../**/*.entity{.ts,.js}'],
   synchronize: true,
   logging: true,
-  dropSchema: false,
+  dropSchema: true,
   subscribers: [__dirname + '/../subscribers/*.subscriber{.ts,.js}'],
 };
